3a331dff822543a65019b8b63582a05bef942fb1
JoKlim <klimesch.jonathan@gmail.com>
2021-01-25 08:57:48 +0100

    Removed all refs to cumulo from bus_test.

--- scripts/data/bus_test.py
+++ scripts/data/bus_test.py
@@ -1,9 +1,10 @@
 import os
+import glob
 import pickle as pkl
 from tqdm import tqdm
-from cumulo.data.loader import TestDataset
 from absl import app
 from absl import flags
+import numpy as np
 
 flags.DEFINE_string('path', None, help='Location of files')
 flags.DEFINE_string('ext', 'npz', help='Extension of files')
@@ -12,6 +13,36 @@ flags.DEFINE_integer('iters', 20, help='Number of iterations')
 flags.DEFINE_integer('fnum', None, help='Number of files')
 flags.DEFINE_integer('stop', None, help='Step for debugging')
 FLAGS = flags.FLAGS
+
+
+def read_npz(npz_file):
+    file = np.load(npz_file)
+    return file['radiances'], file['labels']
+
+
+class TestDataset:
+
+    def __init__(self, d_path: str, ext: str = "npz"):
+        self.root_dir = d_path
+        self.file_paths = glob.glob(os.path.join(d_path, f"*.{ext}"))
+        self.ext = ext
+
+    def get_files(self):
+        return self.file_paths
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, idx):
+        filename = self.file_paths[idx]
+        if self.ext == "npz":
+            radiances, labels = read_npz(filename)
+        elif self.ext == "pkl":
+            with open(filename, 'rb') as f:
+                sample = pkl.load(f)
+            radiances = sample[0]
+            labels = sample[1]
+        return radiances, labels[..., 0], filename
 
 
 def bus_test(path: str, out_path: str):
