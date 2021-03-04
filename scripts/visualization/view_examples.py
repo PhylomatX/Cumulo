f413ad948e26e50cfa96534eac3f7233996b1f8b
JoKlim <klimesch.jonathan@gmail.com>
2021-03-04 11:53:34 +0100

    Updated viewing scripts.

--- scripts/visualization/view_examples.py
+++ scripts/visualization/view_examples.py
@@ -4,11 +4,14 @@ from absl import app
 from absl import flags
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
+from cumulo.utils.utils import include_cloud_mask
 
 flags.DEFINE_string('path', None, help='Directory where npz files are located.')
 flags.DEFINE_string('type', 'train', help='Type of example (val or train)')
 flags.DEFINE_list('sample_interval', [0, 6], help='Interval of files to show from each example.')
 flags.DEFINE_list('epoch_interval', [0, 100], help='Interval of files to show from each example.')
+flags.DEFINE_boolean('save', False, help='Save examples as images')
 FLAGS = flags.FLAGS
 
 
@@ -27,17 +30,34 @@ def main(_):
         file = file_dict[epoch]
         data = np.load(os.path.join(FLAGS.path, file))
         labels = data['labels']
-        outputs = data['outputs']
-        cloud_mask = data['cloud_mask']
+        outputs = data['predictions'] + 1
+        # cloud_mask = data['cloud_mask']
+        # merged = include_cloud_mask(labels, cloud_mask)
+        merged = labels
+        cmap = colors.ListedColormap(['k', 'whitesmoke', 'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple',
+                                      'tab:brown', 'tab:pink', 'dimgrey', 'tab:olive', 'tab:cyan'])
         for i in range(sample_interval[0], sample_interval[1]):
-            fig, axs = plt.subplots(1, 3, figsize=(20, 10))
-            axs[0].imshow(outputs[i])
-            axs[1].imshow(labels[i])
-            axs[2].imshow(cloud_mask[i])
-            axs[0].set_title(f'output - {file}_{i}')
-            axs[1].set_title('label')
-            axs[2].set_title('cloud_mask')
-            plt.show()
+            if not FLAGS.save:
+                fig, axs = plt.subplots(1, 2, figsize=(20, 10))
+                axs[0].imshow(outputs[i], vmin=0, vmax=10, cmap=cmap)
+                axs[1].imshow(merged[i]+1, vmin=0, vmax=10, cmap=cmap)
+                axs[0].set_title(f'output - {file}_{i}')
+                axs[1].set_title('label')
+                plt.show()
+            else:
+                figsize = (15, 12)
+                fig, axs = plt.subplots(1, 1, figsize=figsize)
+                plt.title('Prediction')
+                plt.imshow(outputs[i], vmin=0, vmax=10, cmap=cmap)
+                plt.axis('off')
+                plt.savefig(os.path.join(FLAGS.path, f'epoch_{epoch}_{FLAGS.type}_pred.png'))
+                plt.close()
+                fig, axs = plt.subplots(1, 1, figsize=figsize)
+                plt.title('Ground Truth')
+                plt.imshow(merged[i]+1, vmin=0, vmax=10, cmap=cmap)
+                plt.axis('off')
+                plt.savefig(os.path.join(FLAGS.path, f'epoch_{epoch}_{FLAGS.type}_gt.png'))
+                plt.close()
 
 
 if __name__ == '__main__':
--- scripts/visualization/view_preds.py
+++ scripts/visualization/view_preds.py
@@ -6,6 +6,8 @@ from absl import flags
 from cumulo.utils.utils import include_cloud_mask
 
 flags.DEFINE_string('path', None, help='Location of predictions')
+flags.DEFINE_boolean('save', False, help='Save predictions as images')
+flags.DEFINE_integer('offset', 0, help='Offset of predictions to border to exclude in view')
 FLAGS = flags.FLAGS
 
 
@@ -17,22 +19,42 @@ def main(_):
         print(file)
         data = np.load(os.path.join(FLAGS.path, file))
         prediction = data['prediction']
-        height = len(prediction[:, 0][prediction[:, 1] != -1])
-        width = len(prediction[0, :][prediction[0, :] != -1])
-        prediction = prediction[:height, :width]
-        labels = data['labels'][:height, :width]
-        cloud_mask = data['cloud_mask'][:height, :width]
+        prediction = prediction[FLAGS.offset:-1-FLAGS.offset, FLAGS.offset:-1-FLAGS.offset]
+        labels = data['labels'][FLAGS.offset:-1-FLAGS.offset, FLAGS.offset:-1-FLAGS.offset]
+        cloud_mask = data['cloud_mask'][FLAGS.offset:-1-FLAGS.offset, FLAGS.offset:-1-FLAGS.offset]
         labels = include_cloud_mask(labels, cloud_mask)
-        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-        axs[0].set_title('Predicted cloud classes')
-        axs[0].imshow(prediction)
-        axs[1].set_title('Ground Truth')
-        axs[1].imshow(labels)
-        prediction[prediction != 0] = 1
-        axs[2].set_title('Predicted cloud mask')
-        axs[2].imshow(prediction)
-        plt.show()
-        plt.close()
+        if not FLAGS.save:
+            fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+            axs[0].set_title('Predicted cloud classes')
+            axs[0].imshow(prediction)
+            axs[1].set_title('Ground Truth')
+            axs[1].imshow(labels)
+            prediction[prediction != 0] = 1
+            axs[2].set_title('Predicted cloud mask')
+            axs[2].imshow(prediction)
+            plt.show()
+            plt.close()
+        else:
+            figsize = (15, 12)
+            fig, axs = plt.subplots(1, 1, figsize=figsize)
+            plt.title('Predicted cloud classes')
+            plt.imshow(prediction)
+            plt.axis('off')
+            plt.savefig(os.path.join(FLAGS.path, file.replace('.npz', '_classpred.png')))
+            plt.close()
+            fig, axs = plt.subplots(1, 1, figsize=figsize)
+            plt.title('Cloud mask (GT)')
+            plt.imshow(cloud_mask)
+            plt.axis('off')
+            plt.savefig(os.path.join(FLAGS.path, file.replace('.npz', '_cloudmaskgt.png')))
+            plt.close()
+            fig, axs = plt.subplots(1, 1, figsize=figsize)
+            prediction[prediction != 0] = 1
+            plt.title('Predicted cloud mask')
+            plt.imshow(prediction)
+            plt.axis('off')
+            plt.savefig(os.path.join(FLAGS.path, file.replace('.npz', '_cloudmaskpred.png')))
+            plt.close()
 
 
 if __name__ == '__main__':
