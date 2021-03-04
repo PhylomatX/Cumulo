Fixed accuracy calculation and updated equi network with padding option.

cumulo/models/unet_weak.py
@@ -61,8 +61,7 @@ class UpconvConcat(nn.Module):
         X1_dim = X1.size()[2]
         X2 = extract_img(X1_dim, X2)
         X1 = torch.cat((X1, X2), dim=1)
-        X1 = self.conv2(X1)
-        return X1
+        return self.conv2(X1)
 
 
 def extract_img(size, in_tensor):
@@ -83,10 +82,13 @@ class UNet_weak(nn.Module):
         self.conv1 = DownConv(in_channels, starting_filters, bn_momentum, padding=padding)
         self.conv2 = DownConv(starting_filters, starting_filters * 2, bn_momentum, padding=padding)
         self.conv3 = DownConv(starting_filters * 2, starting_filters * 4, bn_momentum, padding=padding)
+
         self.conv4 = ConvConv(starting_filters * 4, starting_filters * 8, bn_momentum, padding=padding)
+
         self.upconv1 = UpconvConcat(starting_filters * 8, starting_filters * 4, bn_momentum, padding=padding)
         self.upconv2 = UpconvConcat(starting_filters * 4, starting_filters * 2, bn_momentum, padding=padding)
         self.upconv3 = UpconvConcat(starting_filters * 2, starting_filters, bn_momentum, padding=padding)
+
         self.conv_out = nn.Conv2d(starting_filters, out_channels, kernel_size=1)
 
     def forward(self, X):

scripts/pipeline/evaluate.py
@@ -42,6 +42,14 @@ def main(_):
     total_c_labels = np.array([])
     total_predictions = np.array([])
     total_c_predictions = np.array([])
+
+    if FLAGS.o_path is None:
+        o_path = FLAGS.path
+    else:
+        if not os.path.exists(FLAGS.o_path):
+            os.makedirs(FLAGS.o_path)
+        o_path = FLAGS.o_path
+
     for file in tqdm(files):
         data = np.load(file)
         labels = data['labels']  # Raw labels without cloud mask
@@ -77,6 +85,8 @@ def main(_):
             report += 'Cloud type eval:\n\n' + ct_cr_txt + '\n\n'
             cf_matrix = sm.confusion_matrix(labels, prediction)
             report += write_confusion_matrix(cf_matrix, get_target_names(labels, prediction, ct_targets)) + '\n\n\n\n'
+        with open(os.path.join(o_path, 'report.txt'), 'w') as f:
+            f.write(report)
     report += '#### TOTAL ####\n\n'
     cm_cr_txt = sm.classification_report(total_c_labels, total_c_predictions, labels=cm_targets)
     report += 'Cloud mask eval:\n\n' + cm_cr_txt + '\n\n'
@@ -86,13 +96,6 @@ def main(_):
     report += 'Cloud type eval:\n\n' + ct_cr_txt + '\n\n'
     cf_matrix = sm.confusion_matrix(total_labels, total_predictions)
     report += write_confusion_matrix(cf_matrix, get_target_names(total_labels, total_predictions, ct_targets))
-
-    if FLAGS.o_path is None:
-        o_path = FLAGS.path
-    else:
-        if not os.path.exists(FLAGS.o_path):
-            os.makedirs(FLAGS.o_path)
-        o_path = FLAGS.o_path
 
     with open(os.path.join(o_path, 'report.txt'), 'w') as f:
         f.write(report)

scripts/pipeline/unet_training.py
@@ -214,7 +214,7 @@ def train(model, m_path, datasets, criterion1, criterion2, optimizer, scheduler,
                     if offset > 0:
                         labels = labels[:, offset:-offset, offset:-offset]
                         cloud_mask = cloud_mask[:, offset:-offset, offset:-offset]
-                    mask = labels >= 0  # get labeled pixels (independent from cloud mask)
+                    mask = labels >= 0 & cloud_mask == 1  # get labeled pixels in cloudy regions
                     loss = 0
                     for ix in range(mask.shape[0]):
                         bmask = mask[ix]
@@ -232,20 +232,21 @@ def train(model, m_path, datasets, criterion1, criterion2, optimizer, scheduler,
                         scheduler.step()
 
                 outputs = outputs.cpu().detach().numpy()
+                labels = labels.cpu().detach().numpy()
+                inputs = inputs.cpu().detach().numpy()
+                cloud_mask = cloud_mask.cpu().detach().numpy()
                 cloud_mask_pred = outputs[:, 0, ...]
                 cloud_mask_pred[cloud_mask_pred < 0.5] = 0
                 cloud_mask_pred[cloud_mask_pred >= 0.5] = 1
                 cloud_class_pred = np.argmax(outputs[:, 1:, ...], axis=1)
                 output = include_cloud_mask(cloud_class_pred, cloud_mask_pred)
-                labels = labels.cpu().detach().numpy()
-                mask = mask.cpu().detach().numpy()
-                inputs = inputs.cpu().detach().numpy()
-                cloud_mask = cloud_mask.cpu().detach().numpy()
+                merged = include_cloud_mask(labels, cloud_mask)
+                mask = merged > 0
 
                 # statistics
                 running_loss += loss.item()
                 print(f"Epoch: {epoch} - Loss: {running_loss / (sample_ix + 1)}")
-                accuracy = float(np.sum(cloud_class_pred[mask] == labels[mask]) / cloud_class_pred[mask].shape)
+                accuracy = float(np.sum(output[mask] == merged[mask]) / output[mask].shape)  # use only labeled pixels in cloudy regions for accuracies
                 running_accuracy += accuracy
 
                 if phase == 'train' and epoch < 2: