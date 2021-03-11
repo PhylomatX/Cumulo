e8a5a52e9f8f01259ec8a77eee7b03883ef6c92a
JoKlim <klimesch.jonathan@gmail.com>
2021-03-11 22:15:35 +0100

    Extended evaluation with matrix plot and cloud mask ROC.

--- scripts/pipeline/evaluate.py
+++ scripts/pipeline/evaluate.py
@@ -5,11 +5,14 @@ import numpy as np
 from absl import app
 from absl import flags
 import sklearn.metrics as sm
+import matplotlib.pyplot as plt
 from cumulo.utils.utils import include_cloud_mask
 
 flags.DEFINE_string('path', None, help='Location of predictions')
 flags.DEFINE_string('o_path', None, help='Save location, defaults to above path')
-flags.DEFINE_bool('full', True, help='Include file-wise evaluation in report.')
+flags.DEFINE_bool('all', True, help='Include file-wise evaluation in report.')
+flags.DEFINE_bool('mask', False, help='Include file-wise cloud mask eval.')
+flags.DEFINE_bool('mask_total', False, help='Include total cloud mask eval.')
 FLAGS = flags.FLAGS
 
 
@@ -35,13 +38,14 @@ def get_target_names(gtl: np.ndarray, hcl: np.ndarray, targets: list) -> list:
 
 def main(_):
     report = ''
-    cm_targets = [0, 1]
-    ct_targets = list(range(9))
+    mask_targets = [0, 1]  # cloud mask targets ('no cloud', 'cloud')
+    class_targets = list(range(9))  # cloud class targets ('no cloud' + 8 different cloud types)
     files = glob.glob(os.path.join(FLAGS.path, "*.npz"))
-    total_labels = np.array([])
-    total_c_labels = np.array([])
-    total_predictions = np.array([])
-    total_c_predictions = np.array([])
+    total_class_labels = np.array([])
+    total_class_predictions = np.array([])
+    total_mask_labels = np.array([])
+    total_mask_predictions = np.array([])
+    total_mask_predictions_raw = np.array([])
 
     if FLAGS.o_path is None:
         o_path = FLAGS.path
@@ -52,50 +56,115 @@ def main(_):
 
     for file in tqdm(files):
         data = np.load(file)
-        labels = data['labels']  # Raw labels without cloud mask
-        cloud_mask = data['cloud_mask']  # Raw cloud mask
-        prediction = data['prediction']  # predictions where cloud mask and labels have been merged
+        labels = data['labels']
+        cloud_mask = data['cloud_mask'].reshape(-1)
+        prediction = data['prediction']
+        mask_prediction_raw = None
 
-        # --- exclude pixels at the borders which were too small for another tile ---
-        valid = prediction != -1
-        cloud_mask = cloud_mask[valid]
-        labels = labels[valid]
-        prediction = prediction[valid]
+        if prediction.ndim == 3:
+            mask_prediction_raw = prediction[0].copy().reshape(-1)
+            prediction[0][prediction[0] < 0.5] = 0
+            prediction[0][prediction[0] >= 0.5] = 1
+            flat = np.argmax(prediction[1:, ...], 0)
+            prediction = include_cloud_mask(flat, prediction[0]).astype(np.int)
 
-        # --- cloud mask evaluation ---
-        c_prediction = prediction.copy()
-        c_prediction[prediction != 0] = 1
-        total_c_labels = np.append(total_c_labels, cloud_mask)
-        total_c_predictions = np.append(total_c_predictions, c_prediction)
-
-        # --- cloud type evaluation ---
-        labels = include_cloud_mask(labels, cloud_mask)
-        mask = np.logical_and(labels != -1, labels != 0)
-        labels = labels[mask]
-        prediction = prediction[mask]
-        total_labels = np.append(total_labels, labels)
-        total_predictions = np.append(total_predictions, prediction)
-        if FLAGS.full:
+        if FLAGS.all:
             report += f"#### {file.replace(FLAGS.path, '')} ####\n\n"
-            cm_cr_txt = sm.classification_report(cloud_mask, c_prediction, labels=cm_targets)
-            report += 'Cloud mask eval:\n\n' + cm_cr_txt + '\n\n'
-            cf_matrix = sm.confusion_matrix(cloud_mask, c_prediction)
-            report += write_confusion_matrix(cf_matrix, get_target_names(cloud_mask, c_prediction, cm_targets)) + '\n\n'
-            ct_cr_txt = sm.classification_report(labels, prediction, labels=ct_targets)
-            report += 'Cloud type eval:\n\n' + ct_cr_txt + '\n\n'
-            cf_matrix = sm.confusion_matrix(labels, prediction)
-            report += write_confusion_matrix(cf_matrix, get_target_names(labels, prediction, ct_targets)) + '\n\n\n\n'
+
+        if FLAGS.mask:
+            # --- cloud mask evaluation ---
+            mask_prediction = prediction.copy()
+            mask_prediction[prediction != 0] = 1
+            mask_prediction = mask_prediction.reshape(-1)
+            if FLAGS.mask_total:
+                total_mask_labels = np.append(total_mask_labels, cloud_mask)
+                total_mask_predictions = np.append(total_mask_predictions, mask_prediction)
+                if mask_prediction_raw is not None:
+                    total_mask_predictions_raw = np.append(total_mask_predictions_raw, mask_prediction_raw)
+            if FLAGS.all:
+                # --- Cloud mask classification report ---
+                mask_report = sm.classification_report(cloud_mask, mask_prediction, labels=mask_targets)
+                report += 'Cloud mask eval:\n\n' + mask_report + '\n\n'
+
+                # --- Cloud mask ROC curve ---
+                fpr, tpr, _ = sm.roc_curve(cloud_mask, mask_prediction_raw)
+                mask_auc = sm.auc(fpr, tpr)
+                mask_roc_disp = sm.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=mask_auc)
+                mask_roc_disp.plot()
+                plt.savefig(os.path.join(o_path, f"{file.replace(FLAGS.path, '')}_mask_roc.png"))
+                plt.close()
+
+                # --- Cloud mask PR curve ---
+                prec, recall, _ = sm.precision_recall_curve(cloud_mask, mask_prediction_raw)
+                mask_pr_disp = sm.PrecisionRecallDisplay(precision=prec, recall=recall)
+                mask_pr_disp.plot()
+                plt.savefig(os.path.join(o_path, f"{file.replace(FLAGS.path, '')}_mask_pr.png"))
+                plt.close()
+
+                # --- Cloud mask histograms (bin number determined with Freedman-Diaconis) ---
+                cloudy = mask_prediction_raw[cloud_mask == 1]
+                not_cloudy = mask_prediction_raw[cloud_mask == 0]
+
+                n = len(cloudy)
+                sorted_cloudy = np.sort(cloudy)
+                q1 = np.median(sorted_cloudy[:int(n/2)])
+                q3 = np.median(sorted_cloudy[-int(n/2):])
+                bin_width = 2 * (q3 - q1) / np.cbrt(n)
+                bin_num = int(1 / bin_width)
+                cloudy_n, _, _ = plt.hist(cloudy, density=True, bins=bin_num, histtype='step')
+
+                n = len(not_cloudy)
+                sorted_not_cloudy = np.sort(not_cloudy)
+                q1 = np.median(sorted_not_cloudy[:int(n/2)])
+                q3 = np.median(sorted_not_cloudy[-int(n/2):])
+                bin_width = 2 * (q3 - q1) / np.cbrt(n)
+                bin_num = int(1 / bin_width)
+                not_cloudy_n, _, _ = plt.hist(not_cloudy, density=True, bins=bin_num, histtype='step')
+
+                plt.xlabel('Network output')
+                plt.ylabel('Normalized counts')
+                plt.tight_layout()
+                plt.savefig(os.path.join(o_path, f"{file.replace(FLAGS.path, '')}_mask_histo.png"))
+                plt.close()
+
+        # --- cloud class evaluation ---
+        labels = include_cloud_mask(labels.reshape(-1), cloud_mask)
+        cloudy_label_mask = np.logical_and(labels != -1, labels != 0)
+        cloudy_labels = labels[cloudy_label_mask]
+        cloudy_predictions = prediction.reshape(-1)[cloudy_label_mask]
+        total_class_labels = np.append(total_class_labels, cloudy_labels)
+        total_class_predictions = np.append(total_class_predictions, cloudy_predictions)
+        if FLAGS.all:
+            class_report = sm.classification_report(cloudy_labels, cloudy_predictions, labels=class_targets, zero_division=0)
+            report += 'Cloud class eval:\n\n' + class_report + '\n\n'
+            class_matrix = sm.confusion_matrix(cloudy_labels, cloudy_predictions)
+            report += write_confusion_matrix(class_matrix, get_target_names(cloudy_labels, cloudy_predictions, class_targets)) + '\n\n\n\n'
+
+        # --- Save intermediate report ---
         with open(os.path.join(o_path, 'report.txt'), 'w') as f:
             f.write(report)
+
     report += '#### TOTAL ####\n\n'
-    cm_cr_txt = sm.classification_report(total_c_labels, total_c_predictions, labels=cm_targets)
-    report += 'Cloud mask eval:\n\n' + cm_cr_txt + '\n\n'
-    cf_matrix = sm.confusion_matrix(total_c_labels, total_c_predictions)
-    report += write_confusion_matrix(cf_matrix, get_target_names(total_c_labels, total_c_predictions, cm_targets)) + '\n\n'
-    ct_cr_txt = sm.classification_report(total_labels, total_predictions, labels=ct_targets)
-    report += 'Cloud type eval:\n\n' + ct_cr_txt + '\n\n'
-    cf_matrix = sm.confusion_matrix(total_labels, total_predictions)
-    report += write_confusion_matrix(cf_matrix, get_target_names(total_labels, total_predictions, ct_targets))
+
+    if FLAGS.mask and FLAGS.mask_total:
+        # --- Total cloud mask evaluation ---
+        mask_report = sm.classification_report(total_mask_labels, total_mask_predictions, labels=mask_targets)
+        report += 'Cloud mask eval:\n\n' + mask_report + '\n\n'
+
+    # --- Total cloud class evaluation ---
+    class_report = sm.classification_report(total_class_labels, total_class_predictions, labels=class_targets, zero_division=0)
+    report += 'Cloud class eval:\n\n' + class_report + '\n\n'
+    class_matrix_normalized = sm.confusion_matrix(total_class_labels, total_class_predictions, normalize='true')
+    class_matrix_disp = sm.ConfusionMatrixDisplay(class_matrix_normalized, display_labels=class_targets)
+    class_matrix_disp.plot(include_values=False, cmap='Reds')
+    plt.savefig(os.path.join(o_path, 'class_matrix_normalized.png'))
+
+    class_matrix = sm.confusion_matrix(total_class_labels, total_class_predictions)
+    class_matrix_disp = sm.ConfusionMatrixDisplay(class_matrix, display_labels=class_targets)
+    class_matrix_disp.plot(cmap='Reds')
+    plt.savefig(os.path.join(o_path, 'class_matrix.png'))
+
+    report += write_confusion_matrix(class_matrix, get_target_names(total_class_labels, total_class_predictions, class_targets))
 
     with open(os.path.join(o_path, 'report.txt'), 'w') as f:
         f.write(report)
