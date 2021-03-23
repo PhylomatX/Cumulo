e5bad4a2b883dcbb13c32a00c0008a40abb411cf
Jonathan Klimesch <klimesch@srvradig5.informatik.tu-muenchen.de>
2021-03-23 09:36:49 +0100

    BatchNorm is no optional.

--- scripts/pipeline/unet_training.py
+++ scripts/pipeline/unet_training.py
@@ -22,25 +22,28 @@ flags.DEFINE_string('d_path', None, help='Data path')
 flags.DEFINE_string('m_path', None, help='Model path')
 flags.DEFINE_string('filetype', 'nc', help='File type for dataset')
 flags.DEFINE_integer('r_seed', 1, help='Random seed')
-flags.DEFINE_integer('nb_epochs', 100, help='Number of epochs')
+flags.DEFINE_integer('nb_epochs', 200, help='Number of epochs')
 flags.DEFINE_integer('num_workers', 4, help='Number of workers for the dataloader.')
-flags.DEFINE_integer('bs', 32, help='Batch size for training and validation.')
-flags.DEFINE_integer('dataset_bs', 32, help='Batch size for training and validation.')
+flags.DEFINE_integer('bs', 1, help='Batch size for training and validation.')
+flags.DEFINE_integer('dataset_bs', 16, help='Batch size for training and validation.')
 flags.DEFINE_integer('tile_num', None, help='Tile number / data set size.')
-flags.DEFINE_integer('tile_size', 128, help='Tile size.')
+flags.DEFINE_integer('tile_size', 256, help='Tile size.')
 flags.DEFINE_integer('nb_classes', 9, help='Number of classes.')
 flags.DEFINE_integer('center_distance', None, help='Distance between base points of tile extraction.')
-flags.DEFINE_bool('val', False, help='Flag for validation after each epoch.')
+flags.DEFINE_bool('val', True, help='Flag for validation after each epoch.')
 flags.DEFINE_string('model', 'weak', help='Option for choosing between UNets.')
-flags.DEFINE_bool('merged', False, help='Flag for indicating use of merged dataset')
-flags.DEFINE_bool('examples', False, help='Save some training examples in each epoch')
+flags.DEFINE_string('norm', 'none', help='Type of normalization, one of [bn, gn, none]')
+flags.DEFINE_bool('merged', True, help='Flag for indicating use of merged dataset')
+flags.DEFINE_bool('examples', True, help='Save some training examples in each epoch')
 flags.DEFINE_bool('local_norm', False, help='Standardize each image channel-wise. If False the statistics of a data subset will be used.')
-flags.DEFINE_integer('examples_num', None, help='How many samples should get saved as example?')
 flags.DEFINE_integer('analysis_freq', 1, help='Validation and example save frequency')
 flags.DEFINE_integer('rot', 2, help='Number of elements in rotation group')
-flags.DEFINE_integer('offset', 0, help='Cropping offset for labels in case of valid convolutions')
+flags.DEFINE_integer('offset', 46, help='Cropping offset for labels in case of valid convolutions')
 flags.DEFINE_integer('padding', 0, help='Padding for convolutions')
-flags.DEFINE_float('augment_prob', 0, help='Augmentation probability')
+flags.DEFINE_integer('class_weight', 2, help='Weight of class loss')
+flags.DEFINE_integer('mask_weight', 1, help='Weight of mask loss')
+flags.DEFINE_integer('auto_weight', 1, help='Weight of auto loss')
+flags.DEFINE_float('augment_prob', 0.5, help='Augmentation probability')
 FLAGS = flags.FLAGS
 
 
@@ -66,12 +69,14 @@ def main(_):
         device = 'cuda'
     print("using GPUs?", torch.cuda.is_available())
 
-    try:
-        class_weights = np.load(os.path.join(FLAGS.d_path, "class-weights.npy"))
-        m = np.load(os.path.join(FLAGS.d_path, "mean.npy"))
-        std = np.load(os.path.join(FLAGS.d_path, "std.npy"))
-    except FileNotFoundError:
-        print("Statistics were not found!")
+    class_weights = np.load(os.path.join(FLAGS.d_path, "class-weights.npy"))
+    m = np.load(os.path.join(FLAGS.d_path, "mean.npy"))
+    std = np.load(os.path.join(FLAGS.d_path, "std.npy"))
+
+    # custom_class_weights = np.ones_like(class_weights)
+    # custom_class_weights[3] *= 10
+    # custom_class_weights[4] /= 10
+    # class_weights *= custom_class_weights
 
     if FLAGS.local_norm:
         print("Using local normalization.")
@@ -92,7 +97,7 @@ def main(_):
         train_idx = np.load(os.path.join(FLAGS.m_path, 'train_idx.npy'))
         val_idx = np.load(os.path.join(FLAGS.m_path, 'val_idx.npy'))
     except FileNotFoundError:
-        train_idx, val_idx, test_idx = np.split(idx, [int(.85 * tile_num), int(.9 * tile_num)])
+        train_idx, val_idx, test_idx = np.split(idx, [int(.40 * tile_num), int(.45 * tile_num)])
         np.save(os.path.join(FLAGS.m_path, 'train_idx.npy'), train_idx)
         np.save(os.path.join(FLAGS.m_path, 'val_idx.npy'), val_idx)
         np.save(os.path.join(FLAGS.m_path, 'test_idx.npy'), test_idx)
@@ -113,9 +118,11 @@ def main(_):
 
     # Prepare model
     if FLAGS.model == 'weak':
-        model = UNet_weak(in_channels=13, out_channels=FLAGS.nb_classes, starting_filters=32, padding=FLAGS.padding)
+        model = UNet_weak(in_channels=13, out_channels=FLAGS.nb_classes, starting_filters=32, padding=FLAGS.padding, norm=FLAGS.norm)
     elif FLAGS.model == 'equi':
-        model = UNet_equi(in_channels=13, out_channels=FLAGS.nb_classes, starting_filters=32, rot=FLAGS.rot)
+        model = UNet_equi(in_channels=13, out_channels=FLAGS.nb_classes, starting_filters=32, padding=FLAGS.padding, norm=FLAGS.norm, rot=FLAGS.rot)
+    else:
+        raise NotImplementedError()
     print('Model initialized!')
     model = model.to(device)
 
@@ -128,12 +135,21 @@ def main(_):
     #     grp['lr'] = 1e-7
     # lr_sched = torch.optim.lr_scheduler.StepLR(optimizer, 100, 2)
 
-    lr_sched = torch.optim.lr_scheduler.CyclicLR(
-        optimizer,
-        base_lr=1.2e-5,  # 1e-6 for weak, 1.2e-5 for equi
-        max_lr=2e-4,  # 1e-4 for weak, 2e-4 for equi
-        cycle_momentum=True if 'momentum' in optimizer.defaults else False
-    )
+    # base_lr and max_lr were found with the experimental procedure from the CyclicLR paper
+    if FLAGS.model == 'weak':
+        lr_sched = torch.optim.lr_scheduler.CyclicLR(
+            optimizer,
+            base_lr=1e-6,
+            max_lr=1e-4,
+            cycle_momentum=True if 'momentum' in optimizer.defaults else False
+        )
+    elif FLAGS.model == 'equi':
+        lr_sched = torch.optim.lr_scheduler.CyclicLR(
+            optimizer,
+            base_lr=1.2e-5,
+            max_lr=2e-4,
+            cycle_momentum=True if 'momentum' in optimizer.defaults else False
+        )
 
     criterion1 = nn.BCEWithLogitsLoss()
     criterion2 = nn.CrossEntropyLoss(weight=class_weights.to(device))
@@ -238,6 +254,14 @@ def train(model, m_path, datasets, criterion1, criterion2, optimizer, scheduler,
                 labels = labels.cpu().detach().numpy()
                 inputs = inputs.cpu().detach().numpy()
                 cloud_mask = cloud_mask.cpu().detach().numpy()
+
+                # save training examples
+                if FLAGS.examples and epoch % FLAGS.analysis_freq == 0:
+                    if sample_ix == 0:
+                        np.savez(os.path.join(FLAGS.m_path, f'examples/{epoch}_{phase}'),
+                                 labels=labels[:1], outputs=outputs[:1, ...],
+                                 cloud_mask=cloud_mask[:1])
+
                 cloud_mask_pred = outputs[:, 0, ...]
                 cloud_mask_pred[cloud_mask_pred < 0.5] = 0
                 cloud_mask_pred[cloud_mask_pred >= 0.5] = 1
@@ -249,31 +273,21 @@ def train(model, m_path, datasets, criterion1, criterion2, optimizer, scheduler,
 
                 # statistics
                 running_loss += loss.item()
-                accuracy_cloud_mask = float(np.sum(cloud_mask_pred.reshape(-1) == cloud_mask.reshape(-1)) / cloud_mask.reshape(-1).shape[0])
-                accuracy_cloud_class = float(np.sum(cloud_class_pred[mask] == labels[mask]) / labels[mask].shape[0])
-                accuracy_cloudy = float(np.sum(output[cloudy] == merged[cloudy]) / merged[cloudy].shape[0])
-                print(f"Epoch: {epoch} - Loss: {running_loss / (sample_ix + 1)} "
-                      f"- Accuracy_cloud_class: {accuracy_cloud_class} "
-                      f"- Accuracy_cloudy: {accuracy_cloudy} "
-                      f"- Accuracy_cloud_mask: {accuracy_cloud_mask}")
-                accuracy_weighted = (accuracy_cloud_mask + 2 * accuracy_cloud_class) / 3
+                mask_accuracy = float(np.sum(cloud_mask_pred.reshape(-1) == cloud_mask.reshape(-1)) / cloud_mask.reshape(-1).shape[0])
+                class_accuracy = float(np.sum(cloud_class_pred[mask] == labels[mask]) / labels[mask].shape[0])
+                cloudy_class_accuracy = float(np.sum(output[cloudy] == merged[cloudy]) / merged[cloudy].shape[0])
+                print(f"Epoch: {epoch} - Loss: {round(running_loss / (sample_ix + 1), 3)} "
+                      f"- Class accuracy: {round(class_accuracy, 3)} "
+                      f"- Mask accuracy: {round(mask_accuracy, 3)} - {FLAGS.m_path}")
+                accuracy_weighted = (mask_accuracy + 2 * class_accuracy) / 3
                 running_accuracy += accuracy_weighted
-                running_accuracy_cloudy += accuracy_cloudy
+                running_accuracy_cloudy += cloudy_class_accuracy
 
                 if phase == 'train' and epoch < 2:
                     training_info[phase + '_running_accuracy'].append(accuracy_weighted)
                     training_info['running_lr'].append(scheduler.get_lr())
                 with open(os.path.join(FLAGS.m_path, 'metrics.pkl'), 'wb') as f:
                     pkl.dump(training_info, f)
-
-                # save training examples
-                if FLAGS.examples and epoch % FLAGS.analysis_freq == 0:
-                    if sample_ix == 0:
-                        examples_num = FLAGS.examples_num
-                        if examples_num is None:
-                            examples_num = inputs.shape[0]
-                        np.savez(os.path.join(FLAGS.m_path, f'examples/{epoch}_{phase}'), inputs=inputs[:examples_num],
-                                 labels=labels[:examples_num], outputs=output[:examples_num], cloud_mask=cloud_mask[:examples_num])
 
             epoch_loss = running_loss / len(datasets[phase])
             epoch_acc = running_accuracy / len(datasets[phase])
@@ -296,17 +310,17 @@ def train(model, m_path, datasets, criterion1, criterion2, optimizer, scheduler,
             if phase == 'val' and best_acc_cloudy < epoch_acc_cloudy:
                 best_acc_cloudy = epoch_acc_cloudy
                 torch.save(model.state_dict(), os.path.join(m_path, f'val_best_cloudy'))
-                torch.save(model.state_dict(), os.path.join(m_path, f'val_best_cloudy_backup'))
+                # torch.save(model.state_dict(), os.path.join(m_path, f'val_best_cloudy_backup'))
             elif phase == 'train' and epoch_loss < best_loss:
                 best_loss = epoch_loss
                 model.eval()
                 torch.save(model.state_dict(), os.path.join(m_path, f'train_best'))
-                torch.save(model.state_dict(), os.path.join(m_path, f'train_best_backup'))
+                # torch.save(model.state_dict(), os.path.join(m_path, f'train_best_backup'))
                 model.train()
             if phase == 'train':
                 model.eval()
                 torch.save(model.state_dict(), os.path.join(m_path, f'last_model'))
-                torch.save(model.state_dict(), os.path.join(m_path, f'last_model_backup'))
+                # torch.save(model.state_dict(), os.path.join(m_path, f'last_model_backup'))
                 model.train()
 
         with open(os.path.join(FLAGS.m_path, 'metrics.pkl'), 'wb') as f:
